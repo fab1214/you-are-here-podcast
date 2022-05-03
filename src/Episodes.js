41c9025f83fb39dba4285c7e76091038803d7197
fab1214 <88295561+fab1214@users.noreply.github.com>
2022-05-03 14:19:28 -0400

    added .env file for API key

--- src/Episodes.js
+++ src/Episodes.js
@@ -1,31 +1,42 @@
-// import React, {useState} from 'react'
-// const API = 'AIzaSyAcGCqx0kg5Xk0_qFOVULEa7y5ACp5tlNA';
-// const channelID = 'UCyUWgAVvyA0_RU0XZ2qON7A';
-// const result = '10';
+// import React, { useState } from "react";
+// const API = "AIzaSyAcGCqx0kg5Xk0_qFOVULEa7y5ACp5tlNA";
+// const channelID = "UCyUWgAVvyA0_RU0XZ2qON7A";
+// const result = "10";
 // const finalURL = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelID}&maxResults=${result}&order=date&key=${API}`;
 // function Episodes() {
+//   const [videos, setVideos] = useState([]);
 
-// const [video, setVideo] = useState([]);
-
-// fetch(finalURL)
+//   fetch(finalURL)
 //     .then((response) => response.json())
 //     .then((result) => {
-//         const videos = result.items.map(obj => obj.id.videoId);
-//         const url="https://www.youtube.com/embed/" + videos;
-//         setVideo(url);
+//       const ids = result.items.map((obj) => obj.id.videoId);
+//       const prefix = "https://www.youtube.com/embed/";
+//       const url = ids.map((id) => prefix + id);
+//       setVideos(url);
+//       console.log(url);
 //     })
 //     .catch((error) => {
-//         console.error(error);
+//       console.error(error);
 //     });
-  
-//     return (
-//     <div>
-//         <h1>Episodes</h1>
-//         <div className="episode container">
-//             <iframe width="560" height="315" src={video} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-//         </div>
-//     </div>
-//   )
-// }
-// export default Episodes
 
+//   return (
+//     <div>
+//       <h1>Episodes</h1>
+//       {Array.from(videos).forEach((video) => {
+//         console.log(video);
+//       })}
+//       <div className="episode container">
+//         <iframe
+//           width="560"
+//           height="315"
+//           src={video}
+//           title="YouTube video player"
+//           frameborder="0"
+//           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+//           allowfullscreen
+//         ></iframe>
+//       </div>
+//     </div>
+//   );
+// }
+// export default Episodes;
--- src/Home.js
+++ src/Home.js
@@ -1,38 +1,87 @@
-import React, {useState} from 'react';
-import './Home.css';
-const API = 'AIzaSyAcGCqx0kg5Xk0_qFOVULEa7y5ACp5tlNA';
-const channelID = 'UCyUWgAVvyA0_RU0XZ2qON7A';
-const result = '1';
-const finalURL = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelID}&maxResults=${result}&order=date&key=${API}`;
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./Home.css";
+
+const channelID = "UCyUWgAVvyA0_RU0XZ2qON7A";
+const result = "1";
+const finalURL = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelID}&maxResults=${result}&order=date&key=${process.env.REACT_APP_API_KEY}`;
 
 function Home() {
+  const [videos, setVideos] = useState([]);
 
-const [video, setVideo] = useState([]);
-
-fetch(finalURL)
-    .then((response) => response.json())
-    .then((result) => {
-        const videos = result.items.map(obj => obj.id.videoId);
-        const url="https://www.youtube.com/embed/" + videos;
-        setVideo(url);
-    })
-    .catch((error) => {
-        console.error(error);
+  const getVideos = () => {
+    axios.get(finalURL).then((response) => {
+      console.log(response);
+      const ids = response.data.items.map((obj) => obj.id.videoId);
+      const prefix = "https://www.youtube.com/embed/";
+      const url = ids.map((id) => prefix + id);
+      console.log(url);
+      setVideos(url);
     });
+  };
+
+  useEffect(() => getVideos(), []);
+
+  // fetch(finalURL)
+  //   .then((response) => response.json())
+  //   .then((result) => {
+  //     const ids = result.items.map((obj) => obj.id.videoId);
+  //     const prefix = "https://www.youtube.com/embed/";
+  //     const url = ids.map((id) => prefix + id);
+  //     setVideos(url);
+  //     console.log(url);
+  //   })
+  //   .catch((error) => {
+  //     console.error(error);
+  //   });
 
   return (
     <div>
-        <div className="header">
-            <h2>This week's newest video</h2>
-        </div>
+      <div className="header">
+        <h2>This week's newest video</h2>
+      </div>
+      {videos.map((video) => {
+        return (
+          <div className="episode container">
+            <iframe
+              width="560"
+              height="315"
+              src={video}
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          </div>
+        );
+      })};
+      {/* {Array.from(videos).forEach((video) => {
+        console.log(video);
         <div className="episode_container">
-            <iframe width="560" height="315" src={video} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        </div>
-        <div className="view-more">
-                <button><a href='https://www.youtube.com/channel/UCyUWgAVvyA0_RU0XZ2qON7A' target="_blank" rel="noreferrer noopener">View more</a></button>
-        </div>
+          <iframe
+            width="560"
+            height="315"
+            src={videos}
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </div>;
+      })} */}
+      <div className="view-more">
+        <button>
+          <a
+            href="https://www.youtube.com/channel/UCyUWgAVvyA0_RU0XZ2qON7A"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            View more
+          </a>
+        </button>
+      </div>
     </div>
-)
+  );
 }
 
-export default Home
+export default Home;
